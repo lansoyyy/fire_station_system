2157422a523c22ae94a6fbd17a5ea85eef4ff294
Lans12345 <olanalans12345@gmail.com>
2024-09-06 19:30:20 +0800

    wip login screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,10 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+              opacity: 0.35,
+              image: AssetImage(
+                'assets/images/bg.jpg',
+              ),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Container(
+            width: 1000,
+            height: 600,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: 350,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
